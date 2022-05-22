caf3a92f0df3051dbf98f513772e651dba088ad3
Rayhan-dev <mohammodrayhan444@gmail.com>
2022-05-23 01:39:18 +0600

    login from signUp form created

--- src/Pages/Login.js
+++ src/Pages/Login.js
@@ -3,10 +3,32 @@ import { Link } from 'react-router-dom';
 
 const Login = () => {
     return (
-        <div className='grid ms:grid-cols-2 lg:grid-cols-2 sm:grid-cols-1'>
-            <div className='text-left'>
+        <div className='grid ms:grid-cols-2 lg:grid-cols-2 sm:grid-cols-1 flex items-center h-screen '>
+            <div className='text-right mr-12'>
                 <h1 class="text-5xl font-bold">Login now!</h1>
-                <p class="py-6">New to RobotoLab? <Link to={'/signUp'}>Register Now</Link></p>
+                <p class="py-6">New to RobotoLab?<span className='text-primary' > <Link to={'/signUp'}>Register Now</Link></span></p>
+            </div>
+            <div className='w-1/2 bg-base-100 p-5 border border-primary rounded-lg'>
+                <form > 
+                    <div class="form-control">
+                        <label class="label">
+                            <span class="label-text ">Email</span>
+                        </label>
+                        <input type="email" placeholder="email" class="input input-bordered text-black" />
+                    </div>
+                    <div class="form-control">
+                        <label class="label">
+                            <span class="label-text ">Password</span>
+                        </label>
+                        <input type="password" placeholder="password" class="input input-bordered text-black" />
+                        <label class="label">
+                            <a href="#" class="label-text-alt link link-hover">Forgot password?</a>
+                        </label>
+                    </div>
+                    <div class="form-control mt-6">
+                        <button class="btn btn-primary">Login</button>
+                    </div>
+                </form>
             </div>
         </div>
     );
--- src/Pages/SignUp.js
+++ src/Pages/SignUp.js
@@ -1,9 +1,41 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const SignUp = () => {
     return (
-        <div>
-            SignUp
+        <div className='grid ms:grid-cols-2 lg:grid-cols-2 sm:grid-cols-1 flex items-center h-screen'>
+            <div className='text-right mr-12'>
+                <h1 class="text-5xl font-bold">Sign Up now!</h1>
+                <p class="py-6">New to RobotoLab?<span className='text-primary' > <Link to={'/signUp'}>Register Now</Link></span></p>
+            </div>
+            <div className='w-1/2 bg-base-100 p-5 border border-primary rounded-lg'>
+                <form className='text-black'>
+                    <div class="form-control">
+                        <label class="label">
+                            <span class="label-text ">Name</span>
+                        </label>
+                        <input  type="text" placeholder="Name" class="input input-bordered text-black" />
+                    </div>
+                    <div class="form-control">
+                        <label class="label">
+                            <span class="label-text ">Email</span>
+                        </label>
+                        <input type="email" placeholder="email" class="input input-bordered text-black" />
+                    </div>
+                    <div class="form-control">
+                        <label class="label">
+                            <span class="label-text ">Password</span>
+                        </label>
+                        <input type="password" placeholder="password" class="input input-bordered text-black" />
+                        <label class="label">
+                            <a href="#" class="label-text-alt link link-hover">Forgot password?</a>
+                        </label>
+                    </div>
+                    <div class="form-control mt-6">
+                        <button class="btn btn-primary">Login</button>
+                    </div>
+                </form>
+            </div>
         </div>
     );
 };
